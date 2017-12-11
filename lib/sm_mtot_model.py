64b6c3354986d852feb46365703b455fcfd53117
dr-guangtou <dr.guangtou@gmail.com>
2017-12-11 14:22:57 -0800

    Song: make parameter names consistent

--- lib/sm_mtot_model.py
+++ lib/sm_mtot_model.py
@@ -3,27 +3,29 @@
 import numpy as np
 
 
-def sm100_from_smtot(smtot, frac_sm100_by_smtot, log_mass=True):
-    """ Calculate sm100, total stellar mass within 100 kpc,
-    from an input total stellar mass ``smtot``,
+def logms_tot_from_logms_halo(logms_halo, frac_tot_by_halo, log_mass=True):
+    """ Calculate logm_tot, total stellar mass of the galaxy (e.g. logms_100kpc),
+    from an input total stellar mass ``logms_halo``, in unit of log10(Msun).
 
-    The variable ``smtot`` includes central galaxy mass and IHM,
+    The variable ``logms_halo`` includes central galaxy mass and IHM,
     and also the sum of all stellar mass in the satellites, including IHM contribution
-    to the satellites. The variable ``frac_sm100_by_smtot`` can be taken directly
+    to the satellites.
+
+    The variable ``frac_tot_by_halo`` can be taken directly
     from UniverseMachine outputs, or modeled independently in some other way, such as
     via a parameterized power law scaling.
 
     Parameters
     ----------
-    smtot : float or ndarray
+    logms_halo : float or ndarray
         Numpy array of shape (num_gals, ) storing the total amount of stellar mass
         in each halo, including total satellite galaxy mass
 
-    frac_sm100_by_smtot : float or ndarray
-        For each model galaxy, ``frac_sm100_by_smtot`` stores the
-        fraction of galaxy within 100 kpc scaled by ``smtot``.
+    frac_tot_by_halo : float or ndarray
+        For each model galaxy, ``frac_tot_by_halo`` stores the
+        fraction of galaxy within 100 kpc scaled by ``logms_halo``.
         In UniverseMachine, ``sm100`` is treated as galaxy mass + IHM,
-        while ``smtot`` needs to be calculated by additionally
+        while ``logms_halo`` needs to be calculated by additionally
         including contributions from all satellites
 
     Returns
@@ -35,11 +37,11 @@ def sm100_from_smtot(smtot, frac_sm100_by_smtot, log_mass=True):
     Examples
     --------
     >>> num_gals = int(1e3)
-    >>> smtot = np.logspace(10, 12.5, num_gals)
-    >>> frac_sm100_by_smtot = 0.5
-    >>> sm100 = sm100_from_smtot(smtot, frac_sm100_by_smtot)
+    >>> logms_halo = np.logspace(10, 12.5, num_gals)
+    >>> frac_tot_by_halo = 0.5
+    >>> sm100 = logms_tot_from_logms_halo(logms_halo, frac_tot_by_halo)
     """
     if log_mass:
-        return (smtot + np.log10(frac_sm100_by_smtot))
+        return (logms_halo + np.log10(frac_tot_by_halo))
     else:
-        return (smtot * frac_sm100_by_smtot)
+        return (logms_halo * frac_tot_by_halo)

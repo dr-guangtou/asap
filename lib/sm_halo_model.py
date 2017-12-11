dfab91c4d65a8bf0bc044bad7ee2eb99012402d2
dr-guangtou <dr.guangtou@gmail.com>
2017-12-11 13:47:38 -0800

    Song: rename sm_tot_model to sm_halo_model

--- lib/sm_halo_model.py
+++ lib/sm_halo_model.py
@@ -3,11 +3,11 @@
 import numpy as np
 
 
-def smtot_from_mhalo_log_linear(log_mhalo,
-                                shmr_a,
-                                shmr_b,
-                                random_scatter_in_dex,
-                                log_mass=True):
+def logms_halo_from_logmh_log_linear(logmh,
+                                     shmr_a,
+                                     shmr_b,
+                                     random_scatter_in_dex,
+                                     log_mass=True):
     r""" Power law model for total stellar mass in a halo as a function of halo mass.
     The output ``smtot`` variable includes stellar mass of the central, the IHM, and
     the also the total galaxy + IHM mass bound in satellites of the halo.
@@ -17,9 +17,10 @@ def smtot_from_mhalo_log_linear(log_mhalo,
 
     Parameters
     ----------
-    mhalo : float or ndarray
+    logmh : float or ndarray
         Float or Numpy array of shape (num_gals, ) of the halo mass of the galaxy
-        in units of Msun (*not* in scaled h=1 units, but instead in "straight up" units
+        in units of log10(Msun)
+        (*not* in scaled h=1 units, but instead in "straight up" units
         calculated assuming little h equals the value appropriate for its cosmology)
 
     shmr_a : float
@@ -37,7 +38,7 @@ def smtot_from_mhalo_log_linear(log_mhalo,
         Float or Numpy array of shape (num_gals, ) storing the total stellar mass
         in the halo, including BCG, IHL and total satellite galaxy mass in units of Msun
     """
-    log_mhalo = log_mhalo if log_mass else np.log10(log_mhalo)
+    logmh = logmh if log_mass else np.log10(logmh)
 
-    return np.random.normal(loc=(shmr_a * log_mhalo + shmr_b),
+    return np.random.normal(loc=(shmr_a * logmh + shmr_b),
                             scale=random_scatter_in_dex)

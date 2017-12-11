dc85d2da9e0edae35ef46e3bc08f0de2e8abf14f
dr-guangtou <dr.guangtou@gmail.com>
2017-12-11 14:49:03 -0800

    Song: make parameter names consistent

--- lib/sm_minn_model.py
+++ lib/sm_minn_model.py
@@ -5,77 +5,78 @@ import numpy as np
 from halotools.empirical_models import conditional_abunmatch
 
 
-def minn_cam(log10_sm100_data,
-             log10_sm10_data,
-             log10_sm100_model,
-             secondary_haloprop,
-             log10_sm100_bins,
-             sigma=0,
-             num_required_gals_per_massbin=25):
+def logms_inn_cam(logms_tot_obs,
+                  logms_inn_obs,
+                  logms_tot_mod,
+                  secondary_haloprop,
+                  logms_tot_bins,
+                  sigma=0,
+                  num_required_gals_per_massbin=25):
     """
-    Conditional Abundance Matching model for inner 10 kpc stellar mass
-    from an input 100 kpc aperture stellar mass.
+    Conditional Abundance Matching model for inner region stellar mass
+    from an input large aperture stellar mass.
 
     Parameters
     ----------
-    log10_sm100_data : ndarray
-        Numpy array of shape (num_gals, ) storing log10(M100) of the HSC
+    logms_tot_obs : ndarray
+        Numpy array of shape (num_gals, ) storing log10(Mtot) of the HSC
         catalog
 
-    log10_sm10_data : ndarray
-        Numpy array of shape (num_gals, ) storing log10(M10) of the HSC catalog
+    logms_inn_obs : ndarray
+        Numpy array of shape (num_gals, ) storing log10(Minn) of the HSC catalog
 
-    log10_sm100_model : ndarray
-        Numpy array of shape (num_halos, ) storing log10(M100) of the
+    logms_tot_mod : ndarray
+        Numpy array of shape (num_halos, ) storing log10(Mtot) of the
         model galaxies
 
     secondary_haloprop : ndarray
         Numpy array of shape (num_halos, ) storing whatever secondary halo
-        property is used to do the CAM modeling of M10 in bins of M100,
+        property is used to do the CAM modeling of Minn in bins of Mtot,
         e.g., ``secondary_haloprop`` could be the in-situ fraction predicted
         by UniverseMachine
 
-    log10_sm100_bins  ndarray
-        Numpy array of shape (num_bin_edges, ) storing the edges of the M100
-        bins used to discretize P(M10 | M100) in both data and model during CAM
+    logms_tot_bins  ndarray
+        Numpy array of shape (num_bin_edges, ) storing the edges of the Mtot
+        bins used to discretize P(Minn | Mtot) in both data and model during CAM
 
     sigma : float, optional
         Level of CAM scatter. Default value is zero for a perfect, monotonic
-        correspondence between M10 and secondary_haloprop at fixed M100
+        correspondence between Minn and secondary_haloprop at fixed M100
 
     num_required_gals_per_massbin : int, optional
         Number of galaxies required in each M100 bin
-        to ensure good characterization of P(M10 | M100).
+        to ensure good characterization of P(Minn | Mtot).
         Default is to require 25 galaxies per bin.
 
     Returns
     -------
-    log10_sm10_model : float or ndarray
+    logms_inn_mod : float or ndarray
         Numpy array of shape (num_halos, ) storing the values of M10 predicted
         by the CAM model
     """
-    msg = ("``log10_sm100_bins`` must strictly span the min/max values of"
-           "``log10_sm100_model``")
-    assert np.all(log10_sm100_model >= log10_sm100_bins[0]), msg
-    assert np.all(log10_sm100_model < log10_sm100_bins[-1]), msg
+    msg = ("``logms_tot_bins`` must strictly span the min/max values of"
+           "``logms_tot_mod``")
+    assert np.all(logms_tot_mod >= logms_tot_bins[0]), msg
+    assert np.all(logms_tot_mod < logms_tot_bins[-1]), msg
 
-    msg2 = "Only {0} galaxies in the log10_sm100_bins bin ({1:.3f}, {2:.3f})"
-    log10_sm10_model = np.zeros_like(secondary_haloprop)
+    msg2 = "Only {0} galaxies in the logms_tot_bins bin ({1:.3f}, {2:.3f})"
+    logms_inn_mod = np.zeros_like(secondary_haloprop)
 
-    for low, high in zip(log10_sm100_bins[:-1], log10_sm100_bins[1:]):
+    for low, high in zip(logms_tot_bins[:-1], logms_tot_bins[1:]):
 
-        data_mask_ibin = (log10_sm100_data >= low) & (log10_sm100_data < high)
+        data_mask_ibin = (logms_tot_obs >= low) & (logms_tot_obs < high)
         num_gals_ibin = np.count_nonzero(data_mask_ibin)
         assert num_gals_ibin > num_required_gals_per_massbin, msg2.format(
                 num_gals_ibin, low, high)
-        data_sm10_ibin = log10_sm10_data[data_mask_ibin]
+        data_sm10_ibin = logms_inn_obs[data_mask_ibin]
 
-        model_mask_ibin = ((log10_sm100_model >= low) &
-                           (log10_sm100_model < high))
+        model_mask_ibin = ((logms_tot_mod >= low) &
+                           (logms_tot_mod < high))
         num_halos_ibin = np.count_nonzero(model_mask_ibin)
+
         if num_halos_ibin > 0:
             sec_haloprop_ibin = secondary_haloprop[model_mask_ibin]
-            log10_sm10_model[model_mask_ibin] = conditional_abunmatch(
+            logms_inn_mod[model_mask_ibin] = conditional_abunmatch(
                     sec_haloprop_ibin, data_sm10_ibin, sigma=sigma)
 
-    return log10_sm10_model
+    return logms_inn_mod

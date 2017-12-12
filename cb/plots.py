92237ea9a2b185dc47d766d2e680676e331a2a77
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2017-12-12 02:07:32 -0800

    Some more analysis

--- cb/plots.py
+++ cb/plots.py
@@ -1,8 +1,11 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+
 def dm_vs_insitu(catalog, ax=None):
     if ax is None:
         _, ax = plt.subplots()
-    ax.scatter(catalog["mp"], catalog["sm"], s=0.1)
+    ax.scatter(catalog["mp"], catalog["sm"], s=0.01)
     ax.set(
             xscale="log",
             yscale="log",
@@ -37,3 +40,33 @@ def dm_vs_all_sm(catalog, ax=None):
             title="Peak halo mass vs total stellar mass",
     )
     return ax
+
+
+def plotly_stuff(data, y_cols):
+    scatters = [
+            go.Scatter(
+                visible = False,
+                x = data[i]["mp"],
+                y = np.sum(np.array([data[i][col] for col in y_cols]), axis=0),
+                mode = 'markers',
+            ) for i in range(len(data))
+    ]
+    scatters[0]["visible"] = True
+
+    steps = []
+    for i in range(len(data)):
+        steps.append({"method": "restyle", "args": ["visible", [i == j for j in range(len(data))]]})
+    sliders = [{"active": 0, "steps": steps}]
+
+    layout = go.Layout(
+        xaxis=dict(
+            type='log',
+            range=[11.9, 16]
+        ),
+        yaxis=dict(
+            type='log',
+            range=[9, 14]
+        ),
+        sliders=sliders,
+    )
+    return go.Figure(data=scatters, layout=layout)

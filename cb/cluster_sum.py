373065320544f865b0edb17d954e934f1c0cd342
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2017-12-14 00:25:34 -0800

    yapfify

--- cb/cluster_sum.py
+++ cb/cluster_sum.py
@@ -1,23 +1,24 @@
 import numpy as np
 
+
 def centrals_with_satellites(centrals, satellites, n):
     """
     Given an array of centrals and satellites where the satellites are sorted by
     "upid" and then "mp" (or relevant mass key), adds the masses of the n
     largest satellites (or n% largest satellites if n < 1) to the centrals.
     """
-    new_centrals = np.copy(centrals) # no mutation!
+    new_centrals = np.copy(centrals)  # no mutation!
     for i, central in enumerate(new_centrals):
         # Remember sats are sorted by "upid" then the sum of stellar masses, ascending
-        sats = satellites[
-            np.searchsorted(satellites["upid"], central["id"], side="left"):
-            np.searchsorted(satellites["upid"], central["id"], side="right")
-            ]
-        sats = sats[::-1][:get_n(n, len(sats))] # Reversed to get the largest
+        sats = satellites[np.searchsorted(
+            satellites["upid"], central["id"], side="left"):np.searchsorted(
+                satellites["upid"], central["id"], side="right")]
+        sats = sats[::-1][:get_n(n, len(sats))]  # Reversed to get the largest
         # Now add satellite stellar mass/formation to new_centrals
         for col in ['sm', 'icl', 'sfr']:
             new_centrals[i][col] += np.sum(sats[col])
     return new_centrals
+
 
 def get_n(n, total_sats):
     # Error if less than 0
--- cb/plots.py
+++ cb/plots.py
@@ -9,15 +9,24 @@ solarMassUnits = r"($M_{\odot}$)"
 # All plotters should plot using log10(value)
 # Whether they take in data in that format or convert it depends so keep track of that
 
-def add_scatter_plot(ax, x, y, label = None):
+
+def add_scatter_plot(ax, x, y, label=None):
     bins = np.arange(np.min(x), np.max(x), 0.2)
-    mean, _, _ = sps.binned_statistic(x, y, statistic = "mean", bins = bins)
-    std, _, _ = sps.binned_statistic(x, y, statistic = np.std, bins = bins)
+    mean, _, _ = sps.binned_statistic(x, y, statistic="mean", bins=bins)
+    std, _, _ = sps.binned_statistic(x, y, statistic=np.std, bins=bins)
 
     bin_midpoints = bins[:-1] + np.diff(bins) / 2
-    ax.errorbar(bin_midpoints, mean, yerr = std, linestyle = "None", marker = ".", label = label, zorder = 3) # https://github.com/matplotlib/matplotlib/issues/1622
+    ax.errorbar(
+        bin_midpoints,
+        mean,
+        yerr=std,
+        linestyle="None",
+        marker=".",
+        label=label,
+        zorder=3)  # https://github.com/matplotlib/matplotlib/issues/1622
 
-def dm_vs_all_sm_error(catalogs, labels = None):
+
+def dm_vs_all_sm_error(catalogs, labels=None):
     fig, ax = plt.subplots()
     fig.set_size_inches(18.5, 10.5)
     label = None
@@ -26,86 +35,87 @@ def dm_vs_all_sm_error(catalogs, labels = None):
         x = np.log10(catalog["mp"]) + peturbation
         y = np.log10(catalog["icl"] + catalog["sm"])
         bins = np.arange(np.min(x), np.max(x), 0.2)
-        std, _, _ = sps.binned_statistic(x, y, statistic = np.std, bins = bins)
+        std, _, _ = sps.binned_statistic(x, y, statistic=np.std, bins=bins)
         bin_midpoints = bins[:-1] + np.diff(bins) / 2
         if labels is not None:
             label = labels[i]
-        plt.plot(bin_midpoints, std, label = label)
-    ax.set (
-            xlabel = r"Peak HM " + solarMassUnits,
-            ylabel = r"SM Scatter (dex)",
-            title = "SM-HM scatter vs Peak HM",
+        plt.plot(bin_midpoints, std, label=label)
+    ax.set(
+        xlabel=r"Peak HM " + solarMassUnits,
+        ylabel=r"SM Scatter (dex)",
+        title="SM-HM scatter vs Peak HM",
     )
     ax.legend()
     return ax
 
-def dm_vs_all_sm(catalog, ax = None):
+
+def dm_vs_all_sm(catalog, ax=None):
     if ax is None:
         _, ax = plt.subplots()
     x = np.log10(catalog["mp"])
     y = np.log10(catalog["icl"] + catalog["sm"])
-    ax.plot(x, y, linestyle = "None", marker = "o", markersize = 0.1)
+    ax.plot(x, y, linestyle="None", marker="o", markersize=0.1)
     ax.set(
-            xlabel = r"Peak HM " + solarMassUnits,
-            ylabel = r"SM (all) " + solarMassUnits,
-            title = "Total SM vs Peak HM",
+        xlabel=r"Peak HM " + solarMassUnits,
+        ylabel=r"SM (all) " + solarMassUnits,
+        title="Total SM vs Peak HM",
     )
     add_scatter_plot(ax, x, y)
     return ax
+
 
 def dm_vs_insitu(catalog, ax=None):
     if ax is None:
         _, ax = plt.subplots()
     x = np.log10(catalog["mp"])
     y = np.log10(catalog["sm"])
-    ax.plot(x, y, linestyle = "None", marker = "o", markersize = 0.1)
+    ax.plot(x, y, linestyle="None", marker="o", markersize=0.1)
     ax.set(
-            xlabel = r"Peak HM " + solarMassUnits,
-            ylabel = r"SM (insitu) " + solarMassUnits,
-            title = "Insitu SM vs Peak HM",
+        xlabel=r"Peak HM " + solarMassUnits,
+        ylabel=r"SM (insitu) " + solarMassUnits,
+        title="Insitu SM vs Peak HM",
     )
     add_scatter_plot(ax, x, y)
     return ax
 
-def dm_vs_exsitu(catalog, ax = None):
+
+def dm_vs_exsitu(catalog, ax=None):
     if ax is None:
         _, ax = plt.subplots()
     x = np.log10(catalog["mp"])
-    y = np.log10(catalog["icl"], where = catalog["icl"] > 0)
-    ax.plot(x, y, linestyle = "None", marker = "o", markersize = 0.1)
+    y = np.log10(catalog["icl"], where=catalog["icl"] > 0)
+    ax.plot(x, y, linestyle="None", marker="o", markersize=0.1)
     ax.set(
-            xlabel = r"Peak HM " + solarMassUnits,
-            ylabel = r"SM (exsitu) " + solarMassUnits,
-            title = "Exsitu SM vs Peak HM",
+        xlabel=r"Peak HM " + solarMassUnits,
+        ylabel=r"SM (exsitu) " + solarMassUnits,
+        title="Exsitu SM vs Peak HM",
     )
     add_scatter_plot(ax, x, y)
     return ax
 
+
 def plotly_stuff(data, y_cols):
     scatters = [
-            go.Scatter(
-                visible = False,
-                x = data[i]["mp"],
-                y = np.sum(np.array([data[i][col] for col in y_cols]), axis=0),
-                mode = 'markers',
-            ) for i in range(len(data))
+        go.Scatter(
+            visible=False,
+            x=data[i]["mp"],
+            y=np.sum(np.array([data[i][col] for col in y_cols]), axis=0),
+            mode='markers',
+        ) for i in range(len(data))
     ]
     scatters[0]["visible"] = True
 
     steps = []
     for i in range(len(data)):
-        steps.append({"method": "restyle", "args": ["visible", [i == j for j in range(len(data))]]})
+        steps.append({
+            "method": "restyle",
+            "args": ["visible", [i == j for j in range(len(data))]]
+        })
     sliders = [{"active": 0, "steps": steps}]
 
     layout = go.Layout(
-        xaxis = dict(
-            type = 'log',
-            range = [11.9, 16]
-        ),
-        yaxis = dict(
-            type = 'log',
-            range = [9, 14]
-        ),
-        sliders = sliders,
+        xaxis=dict(type='log', range=[11.9, 16]),
+        yaxis=dict(type='log', range=[9, 14]),
+        sliders=sliders,
     )
-    return go.Figure(data = scatters, layout = layout)
+    return go.Figure(data=scatters, layout=layout)

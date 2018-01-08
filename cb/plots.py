f33701d50e0247eeb1923976b0741eca934b26a5
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-01-08 11:56:26 -0800

    post group meeting notes (and plots I did this morning)

--- cb/plots.py
+++ cb/plots.py
@@ -25,6 +25,80 @@ def add_mean_and_stddev(ax, x, y, label=None):
         label=label,
         zorder=3)  # https://github.com/matplotlib/matplotlib/issues/1622
 
+def age_vs_scatter(centrals):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(18.5, 10.5)
+
+    halo_masses = np.log10(centrals["mp"])
+    stellar_masses = np.log10(centrals["icl"] + centrals["sm"])
+    smhm = stellar_masses / halo_masses
+    age = centrals["Halfmass_Scale"]
+
+    x_bin_edges = np.linspace(np.min(halo_masses), np.max(halo_masses), num=16 + 1)
+    y_bin_edges = np.geomspace(np.min(age), np.max(age), num = 16 + 1)
+    heat, x_edge, y_edge, _ = scipy.stats.binned_statistic_2d(
+            halo_masses,
+            age,
+            smhm,
+            bins=[x_bin_edges, y_bin_edges],
+            statistic="std",
+    )
+    heat[heat == 0] = -np.inf
+    image = ax.imshow(
+            heat.T,
+            origin="lower",
+            extent=[x_edge[0], x_edge[-1], y_edge[0], y_edge[-1]],
+            aspect="auto",
+    )
+    ax.set(
+            xticks=x_bin_edges,
+            yticks=np.linspace(np.min(y_bin_edges), np.max(y_bin_edges), num=16 + 1),
+            yticklabels=["{0:.2f}".format(i) for i in y_bin_edges],
+            xlabel=r"log $M_{vir}$" + solarMassUnits,
+            ylabel=r"Scale factor at half mass",
+            title="SMHM variance in HM and age bins",
+    )
+    fig.colorbar(image, label="SMHM variance")
+    return ax
+
+
+def concentration_vs_scatter(centrals):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(18.5, 10.5)
+
+    halo_masses = np.log10(centrals["mp"])
+    stellar_masses = np.log10(centrals["icl"] + centrals["sm"])
+    smhm = stellar_masses / halo_masses
+    concentrations = centrals["rvir"] / centrals["rs"]
+
+    x_bin_edges = np.linspace(np.min(halo_masses), np.max(halo_masses), num=16 + 1)
+    y_bin_edges = np.geomspace(np.min(concentrations), np.max(concentrations), num = 16 + 1)
+    heat, x_edge, y_edge, _ = scipy.stats.binned_statistic_2d(
+            halo_masses,
+            concentrations,
+            smhm,
+            bins=[x_bin_edges, y_bin_edges],
+            statistic="std",
+    )
+    heat[heat == 0] = -np.inf
+    image = ax.imshow(
+            heat.T,
+            origin="lower",
+            extent=[x_edge[0], x_edge[-1], y_edge[0], y_edge[-1]],
+            aspect="auto",
+    )
+    ax.set(
+            xticks=x_bin_edges,
+            yticks=np.linspace(np.min(y_bin_edges), np.max(y_bin_edges), num=16 + 1),
+            yticklabels=["{0:.2f}".format(i) for i in y_bin_edges],
+            xlabel=r"log $M_{vir}$" + solarMassUnits,
+            ylabel=r"Concentration",
+            title="SMHM variance in HM and concentration bins",
+    )
+    fig.colorbar(image, label="SMHM variance")
+    return ax
+
+
 
 def richness_vs_scatter(centrals, satellites, min_mass_for_richness):
     fig, ax = plt.subplots()
@@ -54,7 +128,7 @@ def richness_vs_scatter(centrals, satellites, min_mass_for_richness):
     ax.set(
             xticks=x_bin_edges,
             yticks=np.linspace(0, 64, num=8),
-            yticklabels=[str(i) for i in y_bin_edges],
+            yticklabels=["{0:.2f}".format(i) for i in y_bin_edges],
             xlabel=r"log $M_{vir}$" + solarMassUnits,
             ylabel=r"Richness",
             title="SMHM variance in HM and Richness bins",

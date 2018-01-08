c5b9a5124e9054bee0fd3c2bd7a8850c5eb8a331
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-01-08 09:38:06 -0800

    richness plot

--- cb/plots.py
+++ cb/plots.py
@@ -35,27 +35,37 @@ def richness_vs_scatter(centrals, satellites, min_mass_for_richness):
     smhm = stellar_masses / halo_masses
     richnesses = cluster_sum.get_richness(centrals, satellites, min_mass_for_richness)
 
-    bins = 16
+    x_bin_edges = np.linspace(np.min(halo_masses), np.max(halo_masses), num=16 + 1)
+    y_bin_edges = np.array([0, 1, 2, 4, 8, 16, 32, 64])
     heat, x_edge, y_edge, bin_number = scipy.stats.binned_statistic_2d(
             halo_masses,
             richnesses,
             smhm,
-            bins=bins,
+            bins=[x_bin_edges, y_bin_edges],
             statistic="std",
     )
+    heat[heat == 0] = -np.inf
     image = ax.imshow(
             heat.T, # WHY???
             origin="lower",
             extent=[x_edge[0], x_edge[-1], y_edge[0], y_edge[-1]],
             aspect="auto",
     )
-    cbar = fig.colorbar(image)
+    ax.set(
+            xticks=x_bin_edges,
+            yticks=np.linspace(0, 64, num=8),
+            yticklabels=[str(i) for i in y_bin_edges],
+            xlabel=r"log $M_{vir}$" + solarMassUnits,
+            ylabel=r"Richness",
+            title="SMHM variance in HM and Richness bins",
+    )
+    fig.colorbar(image, label="SMHM variance")
 
     # Lets also get a sense of how much data we have in each bin
     fig2, ax2 = plt.subplots()
     x_ind, y_ind = np.unravel_index(bin_number,
                                 (len(x_edge) + 1, len(y_edge) + 1))
-    bin_counts, _, _= np.histogram2d(x_ind, y_ind, bins=bins)
+    bin_counts, _, _= np.histogram2d(x_ind, y_ind, bins=[len(x_bin_edges)-1, len(y_bin_edges)-1])
     bin_counts = np.log10(bin_counts)
     image = ax2.imshow(
             bin_counts.T,
@@ -64,7 +74,7 @@ def richness_vs_scatter(centrals, satellites, min_mass_for_richness):
             aspect="auto",
     )
 
-    cbar = fig2.colorbar(image)
+    fig2.colorbar(image)
     return ax
 
 

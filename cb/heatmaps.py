116d38edb8aa5462fc76c7aa134a4d180466fbc2
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-01-17 13:57:08 -0800

    almost ready to share

--- cb/heatmaps.py
+++ cb/heatmaps.py
@@ -6,53 +6,47 @@ import cluster_sum
 import plots
 import smhm_fit
 
-def age_vs_scatter(centrals):
-    fig, ax = plt.subplots()
-    fig.set_size_inches(18.5, 10.5)
-
-    # Define the basic quantities we are interested in
+def age_and_hm_vs_sm_scatter(centrals, fit):
     halo_masses = np.log10(centrals["m"])
-    smhm_ratio = np.log10(
-            (centrals["icl"] + centrals["sm"]) / centrals["m"]
-    )
+    stellar_masses = np.log10(centrals["icl"] + centrals["sm"])
     ages = centrals["Halfmass_Scale"]
-
-    # Bin based on halo mass on the x axis and age on the y axis
-    # Calculate the std-dev (scatter) of the smhm_ratio in each 2d bin
     x_bin_edges = np.arange(
             np.floor(10*np.min(halo_masses))/10, # round down to nearest tenth
             np.max(halo_masses) + 0.2, # to ensure that the last point is included
             0.2)
     y_bin_edges = np.linspace(np.min(ages), np.max(ages), num = 16 + 1)
-    binned_stats = scipy.stats.binned_statistic_2d(
-            halo_masses,
-            ages,
-            smhm_ratio,
-            bins=[x_bin_edges, y_bin_edges],
-            statistic="std",
-    )
-
-    # Invalidate bins that don't have many members
-    binned_stats = invalidate_unoccupied_bins(binned_stats)
-
-    # Plot and add labels, colorbar, etc
-    image = ax.imshow(
-            binned_stats.statistic.T,
-            origin="lower",
-            extent=[binned_stats.x_edge[0], binned_stats.x_edge[-1], binned_stats.y_edge[0], binned_stats.y_edge[-1]],
-            aspect="auto",
-    )
+    fig, ax, image, binned_stats = generalised_heatmap(halo_masses, ages, stellar_masses, True, fit, x_bin_edges, y_bin_edges)
     ax.set(
             xticks=binned_stats.x_edge[::2],
             xticklabels=["{0:.1f}".format(i) for i in binned_stats.x_edge[::2]],
             yticks=np.linspace(np.min(binned_stats.y_edge), np.max(binned_stats.y_edge), num=len(binned_stats.y_edge)),
             yticklabels=["{0:.2f}".format(i) for i in binned_stats.y_edge],
             xlabel=plots.m_vir_x_axis,
-            ylabel=r"Scale factor when halo is at half its current mass",
+            ylabel=r"Scale factor at halo half mass",
     )
-    fig.colorbar(image, label=plots.smhm_ratio_scatter)
+    fig.colorbar(image, label=plots.sm_scatter)
     return ax
 
+def age_and_sm_vs_hm_scatter(centrals, fit):
+    halo_masses = np.log10(centrals["m"])
+    stellar_masses = np.log10(centrals["icl"] + centrals["sm"])
+    ages = centrals["Halfmass_Scale"]
+    x_bin_edges = np.arange(
+            np.floor(10*np.min(stellar_masses))/10, # round down to nearest tenth
+            np.max(stellar_masses) + 0.2, # to ensure that the last point is included
+            0.2)
+    y_bin_edges = np.linspace(np.min(ages), np.max(ages), num = 16 + 1)
+    fig, ax, image, binned_stats = generalised_heatmap(stellar_masses, ages, halo_masses, False, fit, x_bin_edges, y_bin_edges)
+    ax.set(
+            xticks=binned_stats.x_edge[::2],
+            xticklabels=["{0:.1f}".format(i) for i in binned_stats.x_edge[::2]],
+            yticks=np.linspace(np.min(binned_stats.y_edge), np.max(binned_stats.y_edge), num=len(binned_stats.y_edge)),
+            yticklabels=["{0:.2f}".format(i) for i in binned_stats.y_edge],
+            xlabel=plots.m_star_x_axis,
+            ylabel=r"Scale factor at halo half mass",
+    )
+    fig.colorbar(image, label=plots.hm_scatter)
+    return ax
 
 def mm_and_hm_vs_sm_scatter(centrals, fit):
     halo_masses = np.log10(centrals["m"])
@@ -90,10 +84,10 @@ def mm_and_sm_vs_hm_scatter(centrals, fit):
             xticklabels=["{0:.1f}".format(i) for i in binned_stats.x_edge[::2]],
             yticks=np.linspace(np.min(binned_stats.y_edge), np.max(binned_stats.y_edge), num=len(binned_stats.y_edge)),
             yticklabels=["{0:.2f}".format(i) for i in binned_stats.y_edge],
-            xlabel=plots.m_vir_x_axis,
+            xlabel=plots.m_star_x_axis,
             ylabel=r"Scale factor at last major merger",
     )
-    fig.colorbar(image, label=plots.sm_scatter)
+    fig.colorbar(image, label=plots.hm_scatter)
     return ax
 
 def concentration_and_hm_vs_sm_scatter(centrals, fit):
@@ -193,7 +187,8 @@ def generalised_heatmap(x_masses, y_quantity, other_masses, x_is_halo, fit, x_bi
         predicted_other_masses = smhm_fit.f_shmr(x_masses, *fit)
     else:
         predicted_other_masses = smhm_fit.f_shmr_inverse(x_masses, *fit)
-    delta_other_masses = other_masses - predicted_other_masses # TODO CBX NOTE THAT THIS IS PROBABLY WRONG
+    delta_other_masses = other_masses - predicted_other_masses
+    assert np.mean(delta_other_masses) < 0.05, "prediction should be ~ the mean"
 
     binned_stats = scipy.stats.binned_statistic_2d(
             x_masses,

24d6bf148d6e0573636d449d22e462937516d678
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-02-27 15:05:43 -0800

    added helpers to smf fit

--- cb/fits/__init__.py
+++ cb/fits/__init__.py
@@ -1,1 +1,1 @@
-from .smf import build_csmf
+from .smf import build_csmf, mass_at_density, density_at_mass
--- cb/fits/smf.py
+++ cb/fits/smf.py
@@ -23,4 +23,28 @@ def build_csmf(catalog, key):
     return bin_centers, _cumulate(y)
 
 
-# TODO - write some helpers that find mass at a given CSMF and vice versa
+def mass_at_density(catalog, key, density):
+    densities = catalog[key]["smf"][1] # monotonically decreasing
+    for i, d in enumerate(densities):
+        if d < density:
+            if i == 0:
+                raise Exception("Given density is higher than everything in our CSMF")
+            index = i
+            break
+    else:
+        raise Exception("Given density is lower than everything in our CSMF")
+
+    return np.mean([catalog[key]["smf"][0][index], catalog[key]["smf"][0][index-1]])
+
+def density_at_mass(catalog, key, mass):
+    masses = catalog[key]["smf"][0] # monotonically increasing
+    for i, m in enumerate(masses):
+        if m > mass:
+            if i == 0:
+                raise Exception("Given mass is lower than everything in our CSMF")
+            index = i
+            break
+    else:
+        raise Exception("Given mass is higher than everything in our CSMF")
+
+    return np.mean([catalog[key]["smf"][1][index], catalog[key]["smf"][1][index-1]])

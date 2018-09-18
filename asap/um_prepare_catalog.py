5561310329a5ddbf5706c2d4d971b79b9f1ac2f5
dr-guangtou <dr.guangtou@gmail.com>
2018-09-18 14:36:33 -0700

    Small updates for the new catalog

--- asap/um_prepare_catalog.py
+++ asap/um_prepare_catalog.py
@@ -4,41 +4,43 @@ This includes codes to value added the catalog and prepare the particle
 table.
 """
 
-from __future__ import print_function
+from __future__ import print_function, division, absolute_import
+
+import copy
 
 from time import time
-from astropy.table import Column
 
 import numpy as np
+from astropy.table import Column
 
 from asap.um_model_predictions import (precompute_lensing_pairs,
                                        total_stellar_mass_including_satellites)
 
 
-def prep_um_catalog(um_mock, um_min_mvir=None, use_mvir=False):
+def prep_um_catalog(um_input, um_min_mvir=None, mhalo_col='logmh_peak'):
     """Prepare the UniverseMachine mock catalog.
 
     The goal is to prepare a FITS catalog that include all
     necessary information.
     During the modeling part, we just need to load this catalog once.
     """
+    # Make a new copy, so don't modify the original catalog.
+    um_mock = copy.deepcopy(um_input)
+
     # Sort the catalog based on the host halo ID
     um_mock.sort('halo_hostid')
 
     # Make a mask for central galaxies
     mask_central = um_mock['upid'] == -1
-    um_mock.add_column(Column(data=mask_central,
-                              name='mask_central'))
+    um_mock.add_column(Column(data=mask_central, name='mask_central'))
 
     # Add a column as the BCG+ICL mass
     um_mock.add_column(Column(data=(um_mock['sm'] + um_mock['icl']),
                               name='mtot_galaxy'))
 
     # Total stellar masses within a halo, including the satellites
-    mstar_mhalo = total_stellar_mass_including_satellites(um_mock,
-                                                          'mtot_galaxy')
-    um_mock.add_column(Column(data=mstar_mhalo,
-                              name='mstar_mhalo'))
+    mstar_mhalo = total_stellar_mass_including_satellites(um_mock, 'mtot_galaxy')
+    um_mock.add_column(Column(data=mstar_mhalo, name='mstar_mhalo'))
 
     # Add log10(Mass)
     # Stellar mass
@@ -61,10 +63,7 @@ def prep_um_catalog(um_mock, um_min_mvir=None, use_mvir=False):
     um_mock.rename_column('host_halo_mvir', 'mhalo_host')
 
     if um_min_mvir is not None:
-        if use_mvir:
-            um_mock_use = um_mock[um_mock['logmh_vir'] >= um_min_mvir]
-        else:
-            um_mock_use = um_mock[um_mock['logmh_host'] >= um_min_mvir]
+        um_mock_use = um_mock[um_mock[mhalo_col] >= um_min_mvir]
     else:
         um_mock_use = um_mock
 
@@ -100,18 +99,16 @@ def precompute_wl_smdpl(um_mock, sim_particles,
 
     """
     n_particles_tot = (n_particles_per_dim ** 3)
+
     if verbose:
         print("#   The simulation particle mass is %f" % m_particle)
-        print("#   The number of particles is %d" %
-              n_particles_tot)
+        print("#   The number of particles is %d" % n_particles_tot)
 
-    sim_downsampling_factor = (n_particles_tot /
-                               float(len(sim_particles)))
+    sim_downsampling_factor = (n_particles_tot / float(len(sim_particles)))
 
     # Radius bins
-    rp_bins = np.logspace(np.log10(wl_min_r),
-                          np.log10(wl_max_r),
-                          wl_n_bins)
+    rp_bins = np.logspace(np.log10(wl_min_r), np.log10(wl_max_r), wl_n_bins)
+
     # Box size
     start = time()
     sim_mass_encl = precompute_lensing_pairs(

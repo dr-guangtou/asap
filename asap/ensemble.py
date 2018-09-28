c24cab8a7cefc0e6a4f9918a071c7c146c9dc055
dr-guangtou <dr.guangtou@gmail.com>
2018-09-28 16:19:46 -0700

    Add setup_moves()

--- asap/ensemble.py
+++ asap/ensemble.py
@@ -12,3 +12,37 @@ except(ImportError):
 from . import io
 
 __all__ = []
+
+
+def setup_moves(cfg_emcee, burnin=False):
+    """Choose the Move object for emcee.
+
+    Parameters
+    ----------
+    cfg_emcee : dict
+        Configuration parameters for emcee
+    burnin : bool, optional
+        Whether this is for burnin
+
+    Return
+    ------
+    emcee_moves : emcee.moves object
+        Move object for emcee walkers.
+
+    """
+    move_col = 'moves' if not burnin else 'moves_burnin'
+
+    if cfg_emcee[move_col] == 'snooker':
+        emcee_moves = emcee.moves.DESnookerMove()
+    elif cfg_emcee[move_col] == 'stretch':
+        emcee_moves = emcee.moves.StretchMove(a=cfg_emcee['stretch_a'])
+    elif cfg_emcee[move_col] == 'walk':
+        emcee_moves = emcee.moves.WalkMove(s=cfg_emcee['walk_s'])
+    elif cfg_emcee[move_col] == 'kde':
+        emcee_moves = emcee.moves.KDEMove()
+    elif cfg_emcee[move_col] == 'de':
+        emcee_moves = emcee.moves.DEMove(cfg_emcee['de_sigma'])
+    else:
+        raise Exception("Wrong option: stretch, walk, kde, de, snooker")
+
+    return emcee_moves

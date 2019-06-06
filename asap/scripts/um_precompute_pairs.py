fc8ed0d3060e66a4354df980d44968172837cb1d
dr-guangtou <dr.guangtou@gmail.com>
2019-06-06 14:54:12 -0700

    Fix typo

--- asap/scripts/um_precompute_pairs.py
+++ asap/scripts/um_precompute_pairs.py
@@ -30,18 +30,18 @@ def main(um_file, ptl_file, wl_min_r=0.08, wl_max_r=50.0, wl_n_bins=22, verbose=
     um_pre, _ = os.path.splitext(um_file)
     ptl_pre, _ = os.path.splitext(ptl_file)
     n_ptl = ptl_pre.split('_')[-1]
-    precompute_out = "{}_{}_r_{:5.2f}_{:5.2f}_{:2d}bins.npy".format(
+    precompute_out = "{}_{}_r_{:4.2f}_{:4.1f}_{:2d}bins.npy".format(
         um_pre, n_ptl, wl_min_r, wl_max_r, wl_n_bins
     )
     if verbose:
         print("# Output file name : {}".format(precompute_out))
 
     # Run precompute
-    if 'smdpl' in um_file:
+    if 'smdpl' in ptl_file:
         mass_encl = vagc.precompute_wl_smdpl(
             um_mock, sim_particles, wl_min_r=wl_min_r, wl_max_r=wl_max_r,
             wl_n_bins=wl_n_bins)
-    elif 'mdpl2' in um_file:
+    elif 'mdpl2' in ptl_file:
         mass_encl = vagc.precompute_wl_mdpl2(
             um_mock, sim_particles, wl_min_r=wl_min_r, wl_max_r=wl_max_r,
             wl_n_bins=wl_n_bins)

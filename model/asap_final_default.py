0f51f57a74d88ed94bde427e2be42a1ad3901fb6
dr-guangtou <dr.guangtou@gmail.com>
2019-06-27 15:37:23 -0700

    Add script to download the model result

--- model/asap_final_default.py
+++ model/asap_final_default.py
@@ -1,5 +1,5 @@
 from asap import fitting
 
-config_file = 'asap_test_test.yaml'
+config_file = 'asap_test_final_6.yaml'
 
 results = fitting.fit_asap_model(config_file, use_global=True, verbose=True)
---
+++ model/get_model_result.sh
@@ -0,0 +1,4 @@
+wget https://www.dropbox.com/s/h8yydkdu89703t4/asap_final_default_burnin_1.npz?dl=0
+wget https://www.dropbox.com/s/xwtw7wfz2xj89z0/asap_final_default_burnin_2.npz?dl=0
+wget https://www.dropbox.com/s/7m04bp6am52ylh4/asap_final_default_burnin_3.npz?dl=0
+wget https://www.dropbox.com/s/mssu11k3uoshw8p/asap_final_default_sample.npz?dl=0

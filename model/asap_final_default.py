5eee9fb6c141fc4fe3388093b112a2378858fc00
dr-guangtou <dr.guangtou@gmail.com>
2019-06-27 15:54:42 -0700

    Clean up the model

--- model/asap_final_default.py
+++ model/asap_final_default.py
@@ -1,5 +1,5 @@
 from asap import fitting
 
-config_file = 'asap_test_final_6.yaml'
+config_file = 'asap_final_default.yaml'
 
 results = fitting.fit_asap_model(config_file, use_global=True, verbose=True)
--- model/asap_test_1.py
+++
@@ -1,5 +0,0 @@
-from asap import fitting
-
-config_file = 'asap_test_1.yaml'
-
-results = fitting.fit_asap_model(config_file, use_global=True, verbose=True)
